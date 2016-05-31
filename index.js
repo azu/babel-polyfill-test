390ef7f5493ddb41909478391b26b5de677bca74
azu <azuciao@gmail.com>
2016-05-31 13:05:49 +0900

    update

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 console.log(Promise);
-require("babel-polyfill");
-
-console.log(Promise);
 
 Promise.resolve().then(() => {
   throw new Error("Test");
 }).catch(error => {
   console.error(error.stack);
+}).then(() => {
+  require("babel-polyfill");
+  console.log("load babel-polyfill");
+  console.log(Promise);
+}).then(() => {
+  return Promise.resolve().then(() => {
+    throw new Error("Test");
+  }).catch(error => {
+    console.error(error.stack);
+  });
 });
